43219f2020d0d4f2c4cd9df594a3ed42c02b375d
Karol.Kuczmarski <devnull@localhost>
2010-08-10 13:47:58 +0200

    Project base structure #3.

--- native-launcher/src/pl/polidea/asl/ASLService.java
+++ native-launcher/src/pl/polidea/asl/ASLService.java
@@ -6,6 +6,17 @@ import android.os.IBinder;
 
 public class ASLService extends Service {
 
+	private bool runExecutable(String path) {
+		try {
+			Class<?> execClass = Class.forName("android.os.Exec");
+			Method createSubprocess = execClass.getMethod("createSubprocess",
+				String.class, String.class, String.class, int[].class);
+			Method waitFor = execClass.getMethod("waitFor", int.class);
+
+
+		}
+	}
+
 	@Override
 	public IBinder onBind(Intent intent) {
 		// TODO Auto-generated method stub
